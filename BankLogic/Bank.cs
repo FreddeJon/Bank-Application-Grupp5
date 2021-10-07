551c694fd89bdcf8e469f95ab9d970465d2e1ace
Mostafa Shubber <m_shubber@hotmail.com>
2021-10-07 12:59:53 +0200

    Added a few methods

--- BankLogic/Bank.cs
+++ BankLogic/Bank.cs
@@ -21,10 +21,56 @@ namespace BankLogic
          * GetCustomerByCustomerID()
          * 
          * AddCustomer()
+         * 
          * RemoveCustomer()
          * 
          * 
          * 
          */
+
+        public bool AddCustomer(long id, string fname, string lname)
+        {
+          var customerid=  CustomerList.FirstOrDefault(x=> x.CustomerID==id);
+
+            if (customerid == null)
+            {
+                CustomerList.Add(new Customer { FirstName=fname, LastName=lname});
+                return true
+            }
+
+            else
+            {
+                return false;
+            }
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            foreach (var customer in CustomerList)
+            {
+                Console.WriteLine($"Firstname: {customer.FirstName} | Lastname: {customer.LastName}");
+            }
+	       return CustomerList;
+        }
+
+        public List<Customer> GetCustomerByCustomerID(long id)
+        {
+            foreach (var customer in CustomerList.Where(c=> c.CustomerID == id))
+            { 
+
+            Console.WriteLine($"Firstname: {customer.FirstName} | Lastname: {customer.LastName}");
+                
+            }
+	         return CustomerList;
+        }
+
+        public List<Customer> RemoveCustomer()
+        {
+            foreach (var customer in CustomerList.Where(c=> c.CustomerID == id))
+            {
+                CustomerList.Remove(customer);
+            }
+	        return CustomerList;
+        }
     }
 }
