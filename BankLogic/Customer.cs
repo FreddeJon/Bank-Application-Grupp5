d2bbbae56937cf6c3a70061ea0e237630b8cf179
Bobbysonhjos <robert.jodelsohn@outlook.com>
2021-10-07 16:01:59 +0200

    Added customer methods!

--- BankLogic/Customer.cs
+++ BankLogic/Customer.cs
@@ -11,20 +11,54 @@ namespace BankLogic
         public string FirstName { get; private set; }
         public string LastName { get; private set; }
         public long CustomerID { get; } //Socialnumber
-        private List<Account> CustomerAccounts { get;  set; }
+        private List<Account> CustomerAccounts { get; set; } = new List<Account>();
+        
 
-         
+        public Customer(string firstname, string lastname, long customerid)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            CustomerID = customerid;
+        }
+        public string GetName()
+        {
+            return FirstName + " " + LastName;
+        }
+        public long GetCustomerID()
+        {
+            return CustomerID;
+        }
+        public List<Account> GetCustomerAccounts()
+        {
+            return CustomerAccounts;
+        }
+        public void ChangeName(string firstname, string lastname)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+        }
+        public void AddAccount()
+        {
+            CustomerAccounts.Add(new Account());
+        }
+
+
+        
+        
+
+
+
         /*
-         * CTOR 
-         * 
-         * GetName()
-         * GetCustomerID()
-         * GetCustomerAccounts()       
          * 
          * 
          * 
          * 
-         * ChangeName();
+         *      
+         * 
+         * 
+         * 
+         * 
+         * 
          * 
          * AddAccount() // To CustomerAccounts link to account ctor
          * 
