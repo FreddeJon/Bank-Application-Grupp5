981127b1aaa97afb6264cd9fb20ce1bacb992408
Amir Husseini <Husseiniamir@hotmail.com>
2021-10-07 18:00:31 +0200

    Created Menu! RemoveCustomer funkade inte!

--- BankUI/Program.cs
+++ BankUI/Program.cs
@@ -12,16 +12,61 @@ namespace BankUI
 
         public static void Main(string[] args)
         {
-
-            Console.WriteLine(Bank.CustomerList.GetHashCode());
-            AddCustomer(); 
-            Console.ReadLine();
-        }
-        public static void AddCustomer()
-        {
             
-            Console.WriteLine(Bank.CustomerList.GetHashCode());
-           
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            string name, lastname;
+            long personnr;
+
+            
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1. Add Customer: ");
+            Console.WriteLine("2. Remove Customer");
+            Console.WriteLine("3. Show All Customers");
+            Console.WriteLine("4. Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine("Please Enter your Name: ");
+                    name = Console.ReadLine();
+
+                    Console.WriteLine("Please Enter your Last Name: ");
+                    lastname = Console.ReadLine();
+
+                    Console.WriteLine("Please Enter your Personnummer: ");
+                    personnr = Convert.ToInt32(Console.ReadLine());
+
+                    Bank.AddCustomer(personnr, name, lastname);
+                    Console.WriteLine("Press a key to continue");
+                    Console.ReadKey();
+                    return true;
+                case "2":
+                    Console.WriteLine("Please Enter Personnummer to remove: ");
+                    personnr = Convert.ToInt32(Console.ReadLine());
+
+                    //Bank.RemoveCustomer(personnr); Funkade inte
+                    Console.WriteLine("Press a key to continue");
+                    Console.ReadKey();
+                    return true;
+                case "3":
+                    Bank.GetCustomers(); 
+                    Console.WriteLine("Press a key to continue");
+                    Console.ReadKey();
+                    return true;
+                case "4":
+                    return false;
+                default:
+                    return true;
+            }
         }
     }
 
