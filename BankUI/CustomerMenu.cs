7cc9e40e90e8cfb1426458bc0bbc34c45af95583
Bobbysonhjos <robert.jodelsohn@outlook.com>
2021-10-08 14:48:57 +0200

    Change

--- BankUI/CustomerMenu.cs
+++ BankUI/CustomerMenu.cs
@@ -9,28 +9,69 @@ namespace BankUI
 {
     class CustomerMenu
     {
-        public void Run()
+        private static Customer CurrentCustomer { get; set; }
+
+
+        public static void Start()
         {
             bool quit = false;
             while (!quit)
             {
                 Console.WriteLine("Enter CustomerID [e] to exit");
-                string userInp = Console.ReadLine().ToLower();
-                if (userInp == "e")    // || or    && and 
+                string customerID = Console.ReadLine().ToLower();
+                if (customerID == "e")    // || or    && and 
                 {
                     quit = true;
                 }
-                else if (userInp.ValidateCustomerID())
+                else if (customerID.ValidateCustomerID())
                 {
-
+                    if (customerID.CustomerExist())
+                    {
+                        CurrentCustomer = Bank.GetCustomerByCustomerID(customerID);
+                        quit = true;
+                        Menu();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Following customer number does not exist");
+                    }
                 }
                 else
                 {
                     Console.WriteLine("Invalid input");
                 }
             }
-         
+        }
+
+
+        private static void Menu()
+        {
+            //AccountMenu
+            //Change name
+            //Delete account
+            //Exit
+            bool quit = false;
+            while (!quit)
+            {
+                Console.WriteLine("1. Show Accounts\n2. Change Name\n3. Delete account\n4. Exit");
+                switch (Console.ReadLine())
+                {
+                    case "1": // AccountMenu finns inte just nu
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4": // Exit
+
+                        break;
+                    default:
+                        break;
+                }quit = true;
+            }
+
         }
 
     }
+
 }
